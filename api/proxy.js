42b908d2b4db7cdf70005bf9943f2fe2859cbb03
puti94 <puti94@foxmail.com>
2021-02-21 11:21:52 +0800

    test proxy

--- api/proxy.js
+++ api/proxy.js
@@ -5,13 +5,7 @@
 
 const https = require('https')
 module.exports = async (req, res) => {
-  const request = https.request({
-    host: "lanhu.oss-cn-beijing.aliyuncs.com",
-    port: "443",
-    path: '/SketchPng60979add04c29b9a6c6ef368e09990f058e5b71d60130e9a85d7e0698bd22e93',
-    method: req.method,
-    // headers: {}
-  })
+  const request = https.get('https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng60979add04c29b9a6c6ef368e09990f058e5b71d60130e9a85d7e0698bd22e93')
   res.pipe(request)
 }
 
