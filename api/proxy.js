32e90daae54f4b44ce06f61c5eb85b30c672ba55
puti94 <puti94@foxmail.com>
2021-02-21 11:45:17 +0800

    test proxy

--- api/proxy.js
+++ api/proxy.js
@@ -3,9 +3,23 @@
  * Time: 2021/2/21 11:00 上午.
  */
 
-const https = require('https')
+const request = require('request')
 module.exports = async (req, res) => {
-  const request = https.get('https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng60979add04c29b9a6c6ef368e09990f058e5b71d60130e9a85d7e0698bd22e93')
-  res.pipe(request)
+  const url = 'https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng60979add04c29b9a6c6ef368e09990f058e5b71d60130e9a85d7e0698bd22e93';
+  const options = {
+    url: url,
+    encoding: null
+  };
+  
+  function callback(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const contentType = response.headers['content-type'];
+      res.setHeader('Content-Type', contentType);
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.end(body);
+    }
+  }
+  
+  request.get(options, callback);
 }
 
